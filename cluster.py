fb984abff30c029fcfa0410f85d883ae56d73da4
camilajaviera <cjmunozn@hotmail.com>
2024-12-03 14:04:59 -0300

    feat(cluster): import the function to collect data and other libraries (pandas, numpy, plt and sklearn.cluster)

---
+++ cluster.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+from dataset import collect_data  # Import the function to collect data
